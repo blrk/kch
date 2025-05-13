aefbd8ffb0a62537affda5c2372c9f3b85aceaf4
blrk <blradhakrishnan@gmail.com>
2025-05-13 16:08:00 +0530

    Add files via upload

---
+++ c3/delete.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Array of namespaces
+namespaces=("mtvlabk8su1" "mtvlabk8su2" "mtvlabk8su3")
+
+ # Pathto your Kubernetes manifest files
+postgres_deployment_file="postgres-deploy.yml" # Make sure these paths are correct
+pvc_file="pvc.yml"
+secret_file="secret.yml"
+
+#set -e # Exit immediately if a command exits with a non-zero status
+
+# Deploy PostgreSQL to the given namespace
+deploy_postgres() {
+  local namespace="$1"
+  echo "Deploying PostgreSQL to namespace '$namespace'..."
+  kubectl delete -f "$pvc_file" -n "$namespace"
+  if [ $? -ne 0 ]; then
+    echo "Failed to delete PVC to namespace ${namespace}"
+  fi
+  kubectl delete -f "$secret_file" -n "$namespace"
+    if [ $? -ne 0 ]; then
+    echo "Failed to delete Secret  to namespace ${namespace}"
+  fi
+  kubectl delete -f "$postgres_deployment_file" -n "$namespace"
+  if [ $? -ne 0 ]; then
+    echo "Failed to delete PostgreSQL Deployment to namespace ${namespace}"
+  fi
+  echo "PostgreSQL deployment started in namespace '$namespace'."
+}
+
+# Main deployment sequence
+for namespace in "${namespaces[@]}"; do
+  deploy_postgres "$namespace"
+done
+
+echo "PostgreSQL deployment process completed in all specified namespaces."
+
